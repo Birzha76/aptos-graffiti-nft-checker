373f42df9a62f72dfad945cfcf8e500612aba267
Artsem Ihnatsiuk <artem.development@gmail.com>
2023-10-24 04:06:59 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Aptos ONE Mainnet Anniversary 2023 NFTs Checker
+
+This script helps you quickly check all your Aptos wallets for Aptos ONE Mainnet Anniversary 2023 NFT on <b>[graffio.art](https://graffio.art/).</b>
+
+### Made by <b>[isartsem](https://t.me/isartem)</b>
+
+## Setup checker
+
+1) Download ZIP and extract it to a folder
+2) Install node.js: `https://nodejs.org/en/` (LTS)
+3) Paste your wallets addresses in `wallets.txt`
+4) Open folder with the script in `cmd`
+```bash
+cd <path to folder with script>
+```
+5) Install dependencies
+```bash
+npm install
+```
+6) Start
+```bash
+node checker
+```
+7) You can see the results in the `results.txt`
---
+++ checker.js
@@ -0,0 +1,48 @@
+import { Provider, Network } from "aptos";
+import fs from 'fs'
+
+
+const parseFile = fileName => fs.readFileSync(fileName, "utf8").split('\n').map(str => str.trim()).filter(str => str.length > 10);
+
+const provider = new Provider(Network.MAINNET)
+const collection_name = "Aptos ONE Mainnet Anniversary 2023"
+
+
+const searchNft = async (client, accountAddress) => {
+    let tokens = await client.getOwnedTokens(accountAddress);
+
+    if (tokens.length === 0) {
+        console.log(`\n${accountAddress} has no tokens.\n`);
+        return;
+    }
+
+    for (let index = 0; index < tokens.current_token_ownerships_v2.length; index++) {
+        const token = tokens.current_token_ownerships_v2[index];
+        if (token.current_token_data.current_collection.collection_name === collection_name) {
+            console.log(`\n[${accountAddress}] NFT Confirmed`);
+            return true;
+        }
+    }
+
+    console.log(`\n[${accountAddress}] No NFT`);
+    return false;
+};
+
+
+(async () => {
+    let wallets = parseFile('wallets.txt')
+    let results = []
+
+    for (let wallet of wallets) {
+        let nftIsset = await searchNft(provider, wallet)
+        results.push([wallet, nftIsset])
+    }
+
+    fs.writeFile('results.txt', results.join('\n'), function(err) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log('[Results Saved] File: results.txt');
+    });
+})()
